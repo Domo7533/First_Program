24afd88764bdc210ee417b48e771a7e442632a0d
Domo <domoereku@gmail.com>
2026-06-18 12:41:07 +0100

    Area Calculator

---
+++ AreaCalculator.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double length = 10;
+        double width = 5;
+
+        double area = length * width;
+
+        Console.WriteLine("Length: " + length);
+        Console.WriteLine("Width: " + width);
+        Console.WriteLine("Area of Rectangle: " + area);
+    }
+}
